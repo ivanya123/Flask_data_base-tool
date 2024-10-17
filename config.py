d0a3029cca0847109b33baf10d8c8873bc5f9819
Ivan <aplesnin2010@yandex.ru>
2024-10-17 11:34:05 +0300

    Удаление скриптов и тестовой базы данных!

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ basedir = os.path.abspath(os.path.dirname('__file__'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'me_app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'DataBase.db')
 
 class TestConfig(Config):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'me_app_test.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'DataBase.db')
     TESTING = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
